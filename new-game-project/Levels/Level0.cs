3d31e3e56b94bf18b557dd046d9bf90fc2eb3171
Kris <fernandoprofitt@gmail.com>
2025-04-04 21:45:34 -0700

    ffasd

--- new-game-project/Levels/Level0.cs
+++ new-game-project/Levels/Level0.cs
@@ -12,5 +12,6 @@ public partial class Level0 : Node3D
     {
         Multiplayer.ConnectedToServer -= SpawnClientPlayer;
         SceneSwitcher.Instance().Network.RpcId(1, "spawn_network_object", new Vector3(0, 20, 0), Vector3.Zero, Basis.Identity, 0, Multiplayer.GetUniqueId());
+        GD.Print("we are calling the RPC");
     }
 }
