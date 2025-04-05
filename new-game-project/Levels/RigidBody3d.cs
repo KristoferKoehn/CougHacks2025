334222f91be632747b5da50345a8e1147d141636
Kris <fernandoprofitt@gmail.com>
2025-04-04 22:44:56 -0700

    fjfjfjf

--- new-game-project/Levels/RigidBody3d.cs
+++ new-game-project/Levels/RigidBody3d.cs
@@ -5,9 +5,14 @@ public partial class RigidBody3d : RigidBody3D
 
     public int multiplayerID = 1;
 
+    public override void _EnterTree()
+    {
+        SetMultiplayerAuthority(multiplayerID);
+    }
+
     public override void _Ready()
     {
-        this.SetMultiplayerAuthority(multiplayerID);
+        
         //SurfaceManager.Instance().CollisionBuildSubscribers.Add(this);
         SurfaceManager.Instance().SubscribeCollisionObject(this);
         GD.Print("spawning test guy");
