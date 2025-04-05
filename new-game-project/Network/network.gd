64d8daa62bd57952cb5a5d8fc363c45eb4842b6b
Kris <fernandoprofitt@gmail.com>
2025-04-04 23:55:43 -0700

    fffffddd

--- new-game-project/Network/network.gd
+++ new-game-project/Network/network.gd
@@ -127,5 +127,7 @@ func rpc_ping(msg : String) -> void:
 func add_player(id: int):
 	print("Adding player %s " % str(id))
 	var p = object_dictionary[0].instantiate()
+	p.name = str(id)
 	p.set_multiplayer_authority(id);
+	
 	network_object_spawn.add_child(p, true)
